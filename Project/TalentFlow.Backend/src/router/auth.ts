977d8018a79638d49d616427780f27ef8fd4c63e
juuhhcarol <juliecaroe17@gmail.com>
2026-06-29 11:01:20 -0300

    Feat: We have begun implementing the routes #13

---
+++ Project/TalentFlow.Backend/src/router/auth.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+
+
+const route = express.Router();
+
+route.post('/login');
+route.post('/logout');
+route.put('/redefinirSenha');
+
+
+export default route;
---
+++ Project/TalentFlow.Backend/src/router/instrutor.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+
+const route = express.Router();
+
+
+route.post('/createprofileinstrutor');
+route.put('/editprofile');
+route.delete('/deleteprofile/:id');
+route 
+
+
+export default route;
