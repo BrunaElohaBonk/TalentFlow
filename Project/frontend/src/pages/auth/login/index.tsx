de49cf0e2009b942a2f2ec51d2f5d8a14a2b1166
BrunaElohaBonk <brunaelohabonk698@gmail.com>
2026-07-06 11:40:44 -0300

    Atualizacao

--- Project/frontend/src/pages/auth/login/index.tsx
+++ Project/frontend/src/pages/auth/login/index.tsx
@@ -1,45 +1,22 @@
-import { useNavigate } from "react-router-dom"
-
+import icon_user from '../../../assets/img/icon_user.png'
+import icon_cadeado from '../../../assets/img/icon_cadeado.png'
 
 function Login() {
 
-  const navigate = useNavigate() 
-
   return (
     <>
-    <div {/*style={{box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px}} */}>
-        <p>Seja bem-vindo!</p>
+    <div>
+        <span>Seja bem-vindo(a)!</span>
 
-        <div style={{height: '50px', width: '100px', border: '3px'}}>
-            <p>EDV</p>
-            <p>Password</p>
+        <div>
+            <input src='icon_user' placeholder="EDV" style={{height: '50px', width: '100px'}}></input>
+            <input src='icon_cadeado' placeholder="Password" style={{height: '50px', width: '100px'}}></input>
         </div>
 
         <div>
             <button>ENTRAR</button>
         </div>
     </div>
-
-
-    {/* <div style={{height: '100vh', width: '100vw', backgroundColor: 'Lavender', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-        <div style={{backgroundColor: 'white', height: '450px', width: '350px', borderRadius: '15px', padding: '20px'}}>
-            <h1 style={{color: 'DarkMagenta', textAlign: 'center', marginTop: '50px'}}>Login</h1><br></br>
-            <div style={{marginTop: '30px'}}>
-                <span style={{fontWeight: 'bold'}}>Nome: </span><br></br>
-                <input placeholder=' Digite o nome:' style={{height: '30px', width: '350px', borderRadius: '5px', border: 0, backgroundColor: 'lavender'}}></input><br></br><br></br>
-                <span style={{fontWeight: 'bold'}}>Email: </span><br></br>
-                <input placeholder=' Digite o email:' style={{height: '30px', width: '350px', borderRadius: '5px', border: 0, backgroundColor: 'lavender'}}></input><br></br><br></br>
-                <span style={{fontWeight: 'bold'}}>Senha: </span><br></br>
-                <input placeholder=' Digite a senha' style={{height: '30px', width: '350px', borderRadius: '5px', border: 0, backgroundColor: 'lavender'}}></input><br></br><br></br>
-            </div>
-            
-            <div style={{display: 'flex', justifyContent: 'space-around', marginTop: '30px'}}>
-              <button onClick={handleNavigateList} style={{height: '50px', width: '100px', backgroundColor: 'DarkMagenta', color: 'white', borderRadius: '10px', border: 0}}>Voltar</button>
-              <button style={{height: '50px', width: '100px', backgroundColor: 'DarkMagenta', color: 'white', borderRadius: '10px', border: 0}}>Entrar</button>
-            </div>
-        </div>
-        <br></br>
-    </div> */}
     </>
   )
 }
