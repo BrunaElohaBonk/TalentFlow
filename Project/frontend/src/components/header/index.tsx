50ce5dc037a035417af4aa8ae7c630a9bcbcc953
Lasnine <lasninemiranda@gmail.com>
2026-07-06 11:42:48 -0300

    Atualizacao

--- Project/frontend/src/components/header/index.tsx
+++ Project/frontend/src/components/header/index.tsx
@@ -1,2 +1,24 @@
 import faixa from "../../assets/img/faixa.png";
 import logo from "../../assets/img/logo.png";
+
+function Header(){
+    return(
+        <div style={quadrado}>
+            <img src={faixa} alt="faixa" />
+            <div>
+                <div>
+
+                </div>
+            </div>
+            <div>
+                <img src={logo} alt="logo" />
+            </div>
+        </div>
+    );
+}
+
+export default Header;
+
+const quadrado = {
+    
+}
---
+++ Project/frontend/src/pages/aprendiz/perfil/index.tsx
@@ -0,0 +1,10 @@
+import Header from "../../../components/header"
+
+function Perfil(){
+    return(
+        <div>
+            <Header></Header>
+        </div>
+    )
+}
+export default Perfil
