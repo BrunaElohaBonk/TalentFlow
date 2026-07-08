1baf1fa72e6621a805bd8b5d9a7fde5693c2e8fc
LaysArceles <LaysArceles12@gmail.com>
2026-07-08 11:43:50 -0300

    feat: create controller auth and prisma #10 #6

--- Project/TalentFlow.Backend/src/Controllers/authController.ts
+++ Project/TalentFlow.Backend/src/Controllers/authController.ts
@@ -1,2 +1,40 @@
-const authController = {};
-export default authController;
+import { Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+import dotenv from "dotenv";
+import { prisma } from "../lib/prisma.ts";
+
+dotenv.config();
+
+class AuthController {
+
+    static async register(req: Request, res: Response): Promise<void> {
+        const data:authDTO = req.body;
+
+        try {
+            const passwordCrypt = await bcrypt.hash(data.password, 10);
+
+            await prisma.intrutor.create ({
+                Data: {
+
+                    name: data.name,
+                    email: data.email,
+                    password: passwordCrypt,
+            }
+
+            });
+
+            await user.save();
+            res.status(201).json({ message: "Usuário cadastrado com sucesso" });
+        } catch (error) {
+            res.status(400).json({ message: "Something failed" });
+        }
+
+    }
+
+    static async login(req: Request, res: Response): Promise<void> {
+        // Implementação da função
+    }
+}
+
+export default AuthController;
