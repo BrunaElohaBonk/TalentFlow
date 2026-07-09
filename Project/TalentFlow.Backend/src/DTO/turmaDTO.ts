b1f6fdbb5ee7cf1f787e780d69b259ed6f7c0057
juuhhcarol <miguel.gradella@aluno.senai.br>
2026-07-09 08:59:29 -0300

    Criação de TurmaDTO #12

---
+++ Project/TalentFlow.Backend/src/DTO/turmaDTO.ts
@@ -0,0 +1,13 @@
+export interface CriarTurmaDTO {
+    nomeTurma:string;
+    curso:string;
+    edvInstrutor: number;
+    nomeInstrutor: string;
+}
+
+export interface EditarTurmaDTO {
+    nomeTurma:string;
+    curso:string;
+    edvInstrutor: number;
+    nomeInstrutor: string;
+}
