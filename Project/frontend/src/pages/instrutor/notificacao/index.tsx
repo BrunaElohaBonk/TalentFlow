848e3ef62d0f21e125c2fc00fe9d866b71f4df6e
Lasnine <lasninemiranda@gmail.com>
2026-07-16 10:32:43 -0300

    Tela de visualizar aprendiz

--- Project/frontend/src/pages/instrutor/notificacao/index.tsx
+++ Project/frontend/src/pages/instrutor/notificacao/index.tsx
@@ -3,7 +3,32 @@ import Sidebar from "../../../components/sidebar"
 import './notificacao.css'
 import { notificacoes } from './notificacao'
 
+interface INotificacao {
+    id: number;
+    nome: string;
+    texto: string;
+    dataHora: string;
+}
+
 function Notificacao(){
+    // const [notificacoes, setNotificacoes] = useState<INotificacao[]>([]);
+    // const fetchNotif = async () => {
+    //     try {
+    //         const response = await axios.get("URL_DO_BACKEND");
+
+    //         console.log(response.data);
+
+    //         setNotificacoes(response.data);
+    //     } catch (error) {
+    //         console.error(error);
+    //         setNotificacoes([]);
+    //     }
+    // };
+
+    // useEffect(() => {
+    //     fetchNotif();
+    // }, []);
+
     return(
         <div>
             <Header></Header>
@@ -12,13 +37,15 @@ function Notificacao(){
                 <div className="notificacao-body">
                     <div className="notificacao-modal">
                         <h1 className="notificacao-titulo">Histórico de Atualização</h1>
+                        <div className="notificacao-lista">
                             {notificacoes.map((item) => (
                                 <div className="notificacao-card" key={item.id}>
-                                    <span className="notificacao-nome">{item.usuario}</span>
-                                    <span className="notificacao-texto">{item.acao}</span>
-                                    <span className="notificacao-texto">{item.data}</span>
+                                    <span className="notificacao-nome">{item.nome}</span>
+                                    <span className="notificacao-texto">{item.texto}</span>
+                                    <span className="notificacao-texto">{item.dataHora}</span>
                                 </div>
                             ))}
+                        </div>
                     </div>
                 </div>
             </div>
